e56d8bd1ac35d32768243296aafd9e28db77fd5e
sudha <sudhapalyal81@gmail.com>
2026-07-25 14:16:35 +0530

    array solution

---
+++ src/String/isomorphicString/arraySol.java
@@ -0,0 +1,39 @@
+package String.isomorphicString;
+
+public class arraySol {
+
+    public static boolean isomorphicCheck(String s, String t) {
+
+        int[] m1 = new int[256];
+        int[] m2 = new int[256];
+        int n = s.length();
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            if (m1[s.charAt(i)] != m2[t.charAt(i)]) {
+                return false;
+            }
+
+            m1[s.charAt(i)] = i + 1;
+            m2[t.charAt(i)] = i + 1;
+        }
+        return true;
+    }
+
+
+
+    public static void main(String[] args){
+
+
+        String s = "egg";
+        String t = "odd0000000";
+
+        boolean ans = isomorphicCheck(s,t);
+        System.out.println(ans);
+    }
+
+}
