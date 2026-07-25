1c7a0a2d315abd2cc889c0f6f44c83ebbdc71b5d
sudha <sudhapalyal81@gmail.com>
2026-07-25 13:34:36 +0530

    HashMap solution

--- src/String/isomorphicString/hashMap.java
+++ src/String/isomorphicString/hashMap.java
@@ -1,4 +1,50 @@
 package String.isomorphicString;
 
+import java.util.HashMap;
+
 public class hashMap {
+
+    public static boolean isomorphicCheck(String s, String t){
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        HashMap<Character, Character> map = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++){
+
+            char original = s.charAt(i);
+            char replacement = t.charAt(i);
+
+            if(!map.containsKey(original)){
+                if(!map.containsValue(replacement)){
+                    map.put(original, replacement);
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                char mappedCharacter = map.get(original);
+                if(mappedCharacter != replacement){
+                    return false;
+                }
+            }
+
+        }
+
+        return true;
+
+    }
+
+    public static void main(String [] args){
+
+        String s = "egg";
+        String t = "odd";
+
+        boolean ans = isomorphicCheck(s,t);
+        System.out.println(ans);
+    }
+
 }
