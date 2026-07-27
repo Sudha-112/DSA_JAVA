9c75eb026a4f8d3e37b8011c6aecbd89aeefac73
sudha <sudhapalyal81@gmail.com>
2026-07-27 15:08:10 +0530

    optimalApp

--- src/String/sortByCharacter/optimalApp.java
+++ src/String/sortByCharacter/optimalApp.java
@@ -1,4 +1,45 @@
 package String.sortByCharacter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 public class optimalApp {
+
+    public static char[] sort(String s) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        List<Character> uniqueChars = new ArrayList<>(map.keySet());
+
+        uniqueChars.sort((a, b) -> {
+            int freqA = map.get(a);
+            int freqB = map.get(b);
+
+            if (freqA != freqB) {
+                return freqB - freqA;  // descending order according to frequency of character
+            }
+            return a - b; //ascending order of characters
+        });
+
+        char[] result = new char[uniqueChars.size()];
+
+        for(int i = 0; i < uniqueChars.size(); i++){
+            result[i] = uniqueChars.get(i);
+        }
+       return result;
+
+    }
+
+    public static void main(String[] args){
+
+        String s = "";
+        char[] ans = sort(s);
+        System.out.println(Arrays.toString(ans));
+    }
 }
