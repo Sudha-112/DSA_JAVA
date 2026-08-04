c5fd058f0640a9b3d3162f250548fc89c4bcdd0c
sudha <sudhapalyal81@gmail.com>
2026-08-04 18:00:19 +0530

    fourSum sol

--- src/Arrayss/FourSum/bruteForce.java
+++ src/Arrayss/FourSum/bruteForce.java
@@ -1,4 +1,42 @@
 package Arrayss.FourSum;
 
+import java.util.*;
+
 public class bruteForce {
+
+    public static List<List<Integer>> findQuadruplets(int[] nums){
+
+        int n = nums.length;
+
+        Set<List<Integer>> resultSet = new HashSet<>();
+        List<List<Integer>> list = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                for(int k =j+1; k < n; k++){
+                    for(int l = k+1; l < n; l++){
+                        if(nums[i] + nums[j] + nums[k] + nums[l] == 0){
+                            resultSet.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                        }
+                    }
+                }
+            }
+        }
+
+        list.addAll(resultSet);
+        return list;
+
+    }
+
+    public static void main(String[] args){
+
+        int [] arr = {1,0,-1,0,-2,2};
+        List<List<Integer>> ans = findQuadruplets(arr);
+        System.out.println(ans);
+    }
 }
+
+//time complexity = O(n^4)
+//space complexity = O(2* no. of unique quadruplets)
--- src/Arrayss/FourSum/optimalApp.java
+++ src/Arrayss/FourSum/optimalApp.java
@@ -1,4 +1,58 @@
 package Arrayss.FourSum;
 
+import java.util.*;
+
 public class optimalApp {
+
+
+    public static List<List<Integer>> findQuadruplets(int[] nums){
+
+        int n = nums.length;
+
+        List<List<Integer>> list = new ArrayList<>();
+
+        Arrays.sort(nums);
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+
+                if(i > 0 && nums[i] == nums[i-1]) continue;
+                if(j > i+1 && nums[j] == nums[j-1]) continue;
+
+                int k = j+1;
+                int l = n-1;
+
+                while(k<l){
+
+                    int sum = nums[i] + nums[j] + nums[k] + nums[l];
+
+                    if(sum == 0){
+                        list.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                        k++;
+                        l--;
+
+                        while(k<l && nums[k] == nums[k-1]) k++;
+                        while(k<l && nums[l] == nums[l+1]) l--;
+
+                    }else if (sum > 0){
+                        l--;
+                    }else{
+                        k++;
+                    }
+                }
+            }
+        }
+
+    return list;
+    }
+
+    public static void main(String[] args){
+
+        int [] arr = {1,0,-1,0,-2,2};
+        List<List<Integer>> ans = findQuadruplets(arr);
+        System.out.println(ans);
+    }
 }
+
+
+// time complexity = O(n^3)
+// space complexity = O(1)
